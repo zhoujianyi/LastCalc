7ea3ccd01af8054c8a678901f0f61a423428a027
DeepReduce <DLR_ICLR20@163.com>
2020-12-23 00:46:35 +0800

    fse

--- src/main/java/com/lastcalc/parsers/bool/IfThenElse.java
+++ src/main/java/com/lastcalc/parsers/bool/IfThenElse.java
@@ -16,11 +16,9 @@
 package com.lastcalc.parsers.bool;
 
 import com.lastcalc.TokenList;
-import com.lastcalc.parsers.Parser;
-import com.lastcalc.parsers.ParserContext;
-import com.lastcalc.parsers.PreParser;
+import com.lastcalc.parsers.*;
 
-public class IfThenElseParser extends Parser {
+public class IfThenElse extends Parser {
 	private static final long serialVersionUID = -7731508022221902181L;
 
 	private static TokenList template = TokenList.createD("if", Boolean.class, "then");
@@ -33,33 +31,27 @@ public class IfThenElseParser extends Parser {
 	@Override
 	public ParseResult parse(final TokenList tokens, final int templatePos, final ParserContext context) {
 		final boolean condition = (Boolean) tokens.get(templatePos + 1);
-		final int endPos = PreParser.findEdgeOrObjectForwards(tokens, templatePos + 2, null) + 1;
+		final int end = PreParser.findEdgeOrObjectForwards(tokens, templatePos + 2, null) + 1;
 		final int elsePos = PreParser.findEdgeOrObjectForwards(tokens, templatePos + 2, "else");
+		if (!tokens.get(elsePos).equals("else"))
+			return ParseResult.fail();
 		TokenList ret;
-		if (!tokens.get(elsePos).equals("else")) {
-			if (condition) {
-				ret = new TokenList.SubTokenList(tokens, templatePos + 3, endPos);
-			} else {
-				return ParseResult.fail();
-			}
+		if (condition) {
+			ret = new TokenList.SubTokenList(tokens, templatePos + 3, elsePos);
 		} else {
-			if (condition) {
-				ret = new TokenList.SubTokenList(tokens, templatePos + 3, elsePos);
-			} else {
-				ret = new TokenList.SubTokenList(tokens, elsePos + 1, endPos);
-			}
+			ret = new TokenList.SubTokenList(tokens, elsePos + 1, end);
 		}
-		return ParseResult.success(tokens.replaceWithTokenList(templatePos, endPos, ret));
+		return ParseResult.success(tokens.replaceWithTokenList(templatePos, end, ret));
 	}
 
 	@Override
 	public int hashCode() {
-		return "IfThenElseParser".hashCode();
+		return "IfThenElse".hashCode();
 	}
 
 	@Override
 	public boolean equals(final Object obj) {
-		return obj instanceof IfThenElseParser;
+		return obj instanceof IfThenElse;
 	}
 
 }
